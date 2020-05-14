31f33d54bec133178e75da03552851c2706dbadb
cian-dooley <60964860+cian-dooley@users.noreply.github.com>
2020-05-14 18:53:53 +0100

    test push

---
+++ Game Tools Project/Assets/scripts/Car_Control.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Control : MonoBehaviour
+{
+    public float MotorForce, SteerForce, Brakeforce;
+    public WheelCollider FR_L_Wheel, FR_R_Wheel, RE_L_Wheel, RE_R_Wheel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical") * MotorForce;
+        float h = Input.GetAxis("Horizontal") * SteerForce;
+
+        RE_R_Wheel.motorTorque = v;
+        RE_L_Wheel.motorTorque = v;
+
+        FR_L_Wheel.steerAngle = h;
+        FR_R_Wheel.steerAngle = h;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            RE_L_Wheel.brakeTorque = Brakeforce;
+            RE_R_Wheel.brakeTorque = Brakeforce;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            RE_L_Wheel.brakeTorque = 0;
+            RE_R_Wheel.brakeTorque = 0;
+        }
+        if(Input.GetAxis("Vertical") == 0)
+        {
+            RE_L_Wheel.brakeTorque = Brakeforce;
+            RE_R_Wheel.brakeTorque = Brakeforce;
+        }
+        else
+        {
+            RE_L_Wheel.brakeTorque = 0;
+            RE_R_Wheel.brakeTorque = 0;
+        }
+
+    }
+}
